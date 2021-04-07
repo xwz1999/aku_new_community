51571d8798398dc93270892008db25742cfcc922
laiiihz <laiiihz@outlook.com>
2021-04-07 14:48:01 +0800

    验证码纯数字键盘

--- lib/pages/sign/sign_in_page.dart
+++ lib/pages/sign/sign_in_page.dart
@@ -8,6 +8,7 @@ import 'package:bot_toast/bot_toast.dart';
 import 'package:dio/dio.dart';
 import 'package:flustars/flustars.dart' show TextUtil;
 import 'package:flustars/flustars.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart' hide Response;
 import 'package:power_logger/power_logger.dart';
@@ -145,6 +146,7 @@ class _SignInPageState extends State<SignInPage> {
     String hint,
     Widget prefix,
     Widget suffix,
+    TextInputType type = TextInputType.number,
     TextEditingController controller,
   }) {
     return Container(
@@ -160,6 +162,11 @@ class _SignInPageState extends State<SignInPage> {
           prefix ?? SizedBox(),
           20.wb,
           TextField(
+            inputFormatters: [
+              FilteringTextInputFormatter.digitsOnly,
+              LengthLimitingTextInputFormatter(11),
+            ],
+            keyboardType: type,
             controller: controller,
             onChanged: (_) => setState(() {}),
             decoration: InputDecoration(
@@ -230,6 +237,7 @@ class _SignInPageState extends State<SignInPage> {
                 ),
                 26.hb,
                 _buildTextField(
+                  type: TextInputType.number,
                   prefix: Image.asset(
                     R.ASSETS_IMAGES_CODE_LOGO_PNG,
                     height: 50.w,
