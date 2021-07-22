0473acee25b57ee470cff2b30292e8da6e0f7eee
zhangmeng <494089941@qq.com>
2021-07-22 15:41:25 +0800

    add patch version tool add version to 1.6.0

--- tool/grind.dart
+++ tool/grind.dart
@@ -13,7 +13,7 @@ part '_project_manage.dart';
 
 main(args) => grind(args);
 
-@Task('add version number')
+@Task('add minor version number')
 void addVersion() async {
   String projectPath = Directory('.').absolute.path;
   String yamlPath = join(projectPath, 'pubspec.yaml');
@@ -23,6 +23,21 @@ void addVersion() async {
   //rename version
 
   Version resultVersion = VersionTool.fromText(version).nextMinorTag('dev');
+
+  String result = yamlContent.replaceFirst(version, resultVersion.toString());
+  await File(yamlPath).writeAsString(result);
+}
+
+@Task('add path version number')
+void addVersionPatch() async {
+  String projectPath = Directory('.').absolute.path;
+  String yamlPath = join(projectPath, 'pubspec.yaml');
+  String yamlContent = await File(yamlPath).readAsString();
+  dynamic content = loadYaml(yamlContent);
+  String version = content['version'];
+  //rename version
+
+  Version resultVersion = VersionTool.fromText(version).nextPatchTag('dev');
 
   String result = yamlContent.replaceFirst(version, resultVersion.toString());
   await File(yamlPath).writeAsString(result);
