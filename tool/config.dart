274f77ecb89887d565b64af0f4f550815ef33124
meng-fucius <494089941@qq.com>
2022-04-11 16:37:48 +0800

    修改弹窗

--- tool/config.dart
+++ tool/config.dart
@@ -1,19 +1,19 @@
 class Config {
   ///用户根目录
-  static const String homeDir = '/users/zhangmeng';
+  static const String homeDir = 'G:\\apk';
 
   ///包名
   static const String packageName = 'aku_new_community';
 
   ///打包目录
   static String get buildPath =>
-      './build/app/outputs/flutter-apk/app-release.apk';
+      '\\build\\app\\outputs\\flutter-apk\\app-release.apk';
 
   ///测试包文件夹
   static String get apkDevDir =>
-      '/users/zhangmeng/team/bee/app/aku_new_community/dev';
+      '$homeDir\\aku_new_community\\dev';
 
   ///正式包文件夹
   static String get apkDir =>
-      '/users/zhangmeng/team/bee/app/aku_new_community/release';
+      '$homeDir\\aku_new_community\\release';
 }
